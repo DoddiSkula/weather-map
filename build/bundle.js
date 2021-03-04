3ae0a3f8c302641ebfaa6f937a172a0fc238094d
Doddi <doddiskula@gmail.com>
2021-03-04 14:10:26 +0000

    Leaflet https

--- build/bundle.js
+++ build/bundle.js
@@ -15482,9 +15482,9 @@
       maxBoundsViscosity: 1.0
     };
     map = leafletSrc.map(mapEl, options);
-    leafletSrc.tileLayer('http://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    leafletSrc.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
       maxZoom: 20,
-      attribution: '&copy; <a href="http://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="http://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+      attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
     }).addTo(map);
   }
 
